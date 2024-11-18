77d76a73b3e931eac7deb9b3649be58883341b04
Adeyemi Micheal Ademola <118080418+Ahdemik@users.noreply.github.com>
2024-11-19 04:15:03 +1300

    Online sales survey Updated.sql

--- Online sales survey.sql
+++ Online sales survey.sql
@@ -1,52 +1,63 @@
---select *
---from [Online Sales Data]
+-- 1. View All Online Sales Data
+SELECT * 
+FROM [Online Sales Data];
 
+-- 2. Total Units Sold
+SELECT SUM(units_sold) AS Total_Units_Sold 
+FROM [Online Sales Data];
 
+-- 3. Total Revenue
+SELECT SUM(Total_Revenue) AS Total_Revenue 
+FROM [Online Sales Data];
 
---Total units sold
-----select sum(units_sold) as Total_units_sold
-----from [Online Sales Data]
+-- 4. Total Revenue by Product Category
+SELECT Product_Category, 
+       SUM(Total_Revenue) AS Total_Revenue_by_Product_Category
+FROM [Online Sales Data]
+GROUP BY Product_Category
+ORDER BY Total_Revenue_by_Product_Category DESC;
 
---Total revenue
-----Select sum(Total_Revenue) as Total_Revenue
-----from [Online Sales Data]
+-- 5. Total Revenue by Region
+SELECT Region, 
+       SUM(Total_Revenue) AS Total_Revenue_per_Region
+FROM [Online Sales Data]
+GROUP BY Region
+ORDER BY Total_Revenue_per_Region DESC;
 
---Total revenue per product category
-----select Product_Category, sum(Total_revenue) as Total_revenue_by_Product_Category
-----from [Online Sales Data]
-----group by Product_Category
-----order by Total_revenue_by_Product_Category desc
+-- 6. Most Purchased Product Category in Asia
+SELECT Product_Category, 
+       COUNT(Product_Category) AS Most_Purchased_Product_Category_In_Asia
+FROM [Online Sales Data]
+WHERE Region LIKE 'Asia'
+GROUP BY Product_Category
+ORDER BY Most_Purchased_Product_Category_In_Asia DESC;
 
-----Total Revenue per region
-----select Region, sum(Total_revenue) as Total_Revenue_per_Region
-----from [Online Sales Data]
-----group by Region
-----order by Total_Revenue_per_Region
+-- 7. Most Purchased Product Category in Europe
+SELECT Product_Category, 
+       COUNT(Product_Category) AS Most_Purchased_Product_Category_In_Europe
+FROM [Online Sales Data]
+WHERE Region LIKE 'Europe'
+GROUP BY Product_Category
+ORDER BY Most_Purchased_Product_Category_In_Europe DESC;
 
-----Most_Purchased_ProductCategory_In_Asia
-----select  product_category, count(product_category) as Most_Purchased_ProductCategory_In_Asia
-----from [Online Sales Data]
-----where Region like 'Asia'
-----group by product_category
+-- 8. Most Purchased Product Category in North America
+SELECT Product_Category, 
+       COUNT(Product_Category) AS Most_Purchased_Product_Category_In_North_America
+FROM [Online Sales Data]
+WHERE Region LIKE 'North America'
+GROUP BY Product_Category
+ORDER BY Most_Purchased_Product_Category_In_North_America DESC;
 
-----Most_Purchased_ProductCategory_In_Europe
-----select  product_category, count(product_category) as Most_Purchased_ProductCategory_In_Europe
-----from [Online Sales Data]
-----where Region like 'Europe'
-----group by product_category
+-- 9. Most Used Payment Method
+SELECT Payment_Method, 
+       COUNT(Payment_Method) AS Most_Used_Payment_Method
+FROM [Online Sales Data]
+GROUP BY Payment_Method
+ORDER BY Most_Used_Payment_Method DESC;
 
-----Most_Purchased_ProductCategory_In_NorthAmerica
-----select  product_category, count(product_category) as Most_Purchased_ProductCategory_In_NorthAmerica
-----from [Online Sales Data]
-----where Region like 'Asia'
-----group by product_category
-
---Most used payment method
-----select Payment_Method, count(Payment_Method) as Most_Used_Payment_Method
-----from [Online Sales Data]
-----group by Payment_Method
-
---select Payment_Method, sum(Total_Revenue) as Total_Revenue_per_Payment_method
---from [Online Sales Data]
---group by Payment_Method
---order by Total_Revenue_per_Payment_method desc
+-- 10. Total Revenue by Payment Method
+SELECT Payment_Method, 
+       SUM(Total_Revenue) AS Total_Revenue_per_Payment_Method
+FROM [Online Sales Data]
+GROUP BY Payment_Method
+ORDER BY Total_Revenue_per_Payment_Method DESC;
